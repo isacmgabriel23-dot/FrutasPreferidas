296bbe3de9c74a5c563f70a9a546eb1fcc901d02
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2026-04-24 19:36:39 -0300

    TRaz a solução do exercício FrutasPreferidas

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("--- Minhas frutas preferidas ---\n");
+Console.WriteLine("1. Abacate");
+Console.WriteLine("2. Pêra");
+Console.WriteLine("3. Laranja");
+Console.WriteLine("4. Manga");
+Console.WriteLine("5. Banana");
